d9d265474bd8a3f712b82cf920b473f79ab18f91
PatriceVignola <vignola.patrice@gmail.com>
2018-03-07 12:52:05 +0900

    Fix some windows not showing in the Suggested Snaps
    Minimized windows and some windows of multi-windows apps were not showing, so I had to rework the algorithm entirely

--- DesktopSplitter/DesktopWindow.swift
+++ DesktopSplitter/DesktopWindow.swift
@@ -14,16 +14,18 @@ class DesktopWindow {
     let title: String
     let screenshot, icon: NSImage
     let isKey: Bool
+    private var isMinimized: Bool
     private let axWindow: AXUIElement
     
     fileprivate init(indexInApp: Int, processId: pid_t, title: String, screenshot: NSImage, icon: NSImage, isKey: Bool,
-                     axWindow: AXUIElement) {
+                     isMinimized: Bool, axWindow: AXUIElement) {
         self.indexInApp = indexInApp
         self.processId = processId
         self.title = title
         self.screenshot = screenshot
         self.icon = icon
         self.isKey = isKey
+        self.isMinimized = isMinimized
         self.axWindow = axWindow
     }
     
@@ -46,6 +48,15 @@ class DesktopWindow {
     
     func bringToFront() {
         AXUIElementSetAttributeValue(axWindow, kAXMainAttribute as CFString, kCFBooleanTrue)
+        set(isMinimized: false)
+    }
+    
+    func set(isMinimized: Bool) {
+        guard self.isMinimized != isMinimized else { return }
+        
+        let cfMinimized: CFBoolean = isMinimized ? kCFBooleanTrue : kCFBooleanFalse
+        AXUIElementSetAttributeValue(axWindow, kAXMinimizedAttribute as CFString, cfMinimized)
+        self.isMinimized = isMinimized
     }
     
     static func getOpenedWindows() -> [DesktopWindow] {
@@ -53,8 +64,8 @@ class DesktopWindow {
         
         var cgProcessWindowsMap = [pid_t : [[String : AnyObject]]]()
         
-        let listOptions = CGWindowListOption.excludeDesktopElements.union(CGWindowListOption.optionOnScreenOnly)
-        let windows = CGWindowListCopyWindowInfo(listOptions, kCGNullWindowID) as! Array<Dictionary<String, AnyObject>>
+        let listOptions = CGWindowListOption.excludeDesktopElements
+        let windows = CGWindowListCopyWindowInfo(listOptions, kCGNullWindowID) as! [[String : AnyObject]]
         
         // Since AXUIElement windows and CGWindows are not linked by a common ID, we need to link them manually
         for window in windows {
@@ -69,52 +80,55 @@ class DesktopWindow {
         
         for app in NSWorkspace.shared.runningApplications {
             guard app.activationPolicy == .regular else { continue }
-            guard let cgWindows = cgProcessWindowsMap[app.processIdentifier] else { continue }
+            guard var cgWindows = cgProcessWindowsMap[app.processIdentifier] else { continue }
             
-            var axWindowsOptional: AnyObject?
-            let axApp = AXUIElementCreateApplication(app.processIdentifier)
-            let axResult = AXUIElementCopyAttributeValue(axApp, kAXWindowsAttribute as CFString, &axWindowsOptional)
-            
-            guard axResult == .success else {
-                if axResult == .apiDisabled {
-                    // TODO: Find the best way to handle the error and retry the operation
-                    NSLog("Enable the accessibility features for this app")
+            let axWindows = getAxWindows(for: app.processIdentifier)
+        
+            for axWindow in axWindows {
+                guard let title = getTitle(from: axWindow) else { continue }
+
+                for i in 0...cgWindows.count - 1 {
+                    let cgWindow = cgWindows[i]
+                    
+                    // We make sure that cgWindow's title is the same as axWindow's
+                    guard title == cgWindow[kCGWindowName as String] as? String else { continue }
+                    
+                    // TODO: Maybe make the icon optional?
+                    guard let icon = app.icon else { continue }
+                    
+                    let windowId = cgWindow[kCGWindowNumber as String] as! CGWindowID
+                    var isMinimized = true
+                    
+                    if let isOnScreen = cgWindow[kCGWindowIsOnscreen as String] {
+                        isMinimized = !(isOnScreen as! Bool)
+                    }
+                    
+                    let windowOptions = CGWindowListOption.optionIncludingWindow
+                    let options = CGWindowImageOption.bestResolution.union(.boundsIgnoreFraming)
+                    let cgScreenshotOptional = CGWindowListCreateImage(CGRect.null, windowOptions, windowId, options)
+                    
+                    // TODO: Maybe make the screenshot optional?
+                    guard let cgScreenshot = cgScreenshotOptional else { continue }
+                    
+                    let screenshotSize = NSSize(width: cgScreenshot.width, height: cgScreenshot.height)
+                    
+                    let openedWindow = DesktopWindow(
+                        indexInApp: i,
+                        processId: app.processIdentifier,
+                        title: title,
+                        screenshot: NSImage(cgImage: cgScreenshot, size: screenshotSize),
+                        icon: icon,
+                        isKey: isKey(axWindow: axWindow, from: app),
+                        isMinimized: isMinimized,
+                        axWindow: axWindow
+                    )
+                     
+                    openedWindows.append(openedWindow)
+                    
+                    cgWindows.remove(at: i)
+                    
+                    break
                 }
-                
-                continue
-            }
-            
-            guard let axWindows = axWindowsOptional as? [AXUIElement] else { continue }
-            
-            for i in 0...cgWindows.count - 1 {
-                let axWindow = axWindows[i]
-                let cgWindow = cgWindows[i]
-                
-                guard let boundsDict = cgWindow[kCGWindowBounds as String] as! CFDictionary? else { continue }
-                
-                let windowId = cgWindow[kCGWindowNumber as String] as! CGWindowID
-                let windowOptions = CGWindowListOption.optionIncludingWindow
-                let bounds = CGRect.init(dictionaryRepresentation: boundsDict)!
-                let resolution = CGWindowImageOption.bestResolution
-                let cgScreenshotOptional = CGWindowListCreateImage(bounds, windowOptions, windowId, resolution)
-                
-                guard let cgScreenshot = cgScreenshotOptional else { continue }
-                guard let title = cgWindow[kCGWindowName as String] as? String, title != "" else { continue }
-                guard let icon = app.icon else { continue }
-                
-                let screenshotSize = NSSize(width: cgScreenshot.width, height: cgScreenshot.height)
-                
-                let openedWindow = DesktopWindow(
-                    indexInApp: i,
-                    processId: app.processIdentifier,
-                    title: title,
-                    screenshot: NSImage(cgImage: cgScreenshot, size: screenshotSize),
-                    icon: icon,
-                    isKey: isKey(axWindow: axWindow, from: app),
-                    axWindow: axWindow
-                )
-                
-                openedWindows.append(openedWindow)
             }
         }
         
@@ -125,5 +139,33 @@ class DesktopWindow {
         var isMainWindow: AnyObject?
         let axResult = AXUIElementCopyAttributeValue(axWindow, kAXMainAttribute as CFString, &isMainWindow)
         return axResult == .success && app.isActive && (isMainWindow as! CFBoolean) == kCFBooleanTrue
+    }
+    
+    private static func getAxWindows(for processId: pid_t) -> [AXUIElement] {
+        var axWindowsOptional: AnyObject?
+        let axApp = AXUIElementCreateApplication(processId)
+        let axResult = AXUIElementCopyAttributeValue(axApp, kAXWindowsAttribute as CFString, &axWindowsOptional)
+        
+        if axResult == .success, let axWindows = axWindowsOptional as? [AXUIElement] {
+            return axWindows
+        }
+        
+        if axResult == .apiDisabled {
+            // TODO: Find the best way to handle the error and retry the operation
+            NSLog("Enable the accessibility features for this app")
+        }
+        
+        return [AXUIElement]()
+    }
+    
+    private static func getTitle(from axWindow: AXUIElement) -> String? {
+        var axTitle: AnyObject?
+        let axResult = AXUIElementCopyAttributeValue(axWindow, kAXTitleAttribute as CFString, &axTitle)
+        
+        if axResult == .success, let axTitleString = axTitle as? String {
+            return axTitleString
+        }
+        
+        return nil
     }
 }
